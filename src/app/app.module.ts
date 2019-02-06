c55fbfb1944146257049a985e46712e71df6f458
Edward Hazell <ehazell95@gmail.com>
2019-02-06 13:44:40 -0500

    made initial componenets and setup basic products json for httpget and build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { ProductComponent } from './components/product/product.component';
+import { ShoppingCartComponent } from './componenets/shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    ProductComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
